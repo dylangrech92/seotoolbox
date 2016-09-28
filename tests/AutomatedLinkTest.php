17fbc13b504102ad40adaf324c313c546e3fb73c
dylangrech92 <dylangrech92@gmail.com>
2016-09-28 08:18:54 +0200

    #7 Functional Tests

--- tests/AutomatedLinkTest.php
+++ tests/AutomatedLinkTest.php
@@ -1,24 +1,84 @@
 <?php
 
+/**
+ * Class AutomatedLinkTest
+ * Run tests to check the Automated Link Object and it's functionality
+ *
+ * @method void assertTrue(boolean $test, string $message)
+ */
 class AutomatedLinkTest extends SapphireTest {
 
     public static $fixture_file = 'fixtures/AutomatedLinkTest.yml';
 
+    /**
+     * Test that links are included in the content field
+     */
     public function testInsertion(){
-        $this->objFromFixture('AutomatedLink','link1');
+        $this->objFromFixture('AutomatedLink','link');
+        $page   = $this->createPage('<p>Checking if phrase is replaced</p>');
+        $link   = $this->getLinkFromPage($page);
+        $this->assertTrue($link->nodeValue === 'phrase', 'Link wasn\'t created');
+    }
 
-        $page   = $this->createPage('test-1', '<p>Checking if phrase is replaced</p>');
+    /**
+     * Test the individual properties of the automated link
+     */
+    public function testProperties(){
+        $this->objFromFixture('AutomatedLink','link2');
+
+        $page   = $this->createPage('<p>Checking if properties is with correct props</p>');
+        $link   = $this->getLinkFromPage($page);
+
+        $this->assertTrue( $link->nodeValue === 'properties', 'Phrase did not match' );
+        $this->assertTrue( $link->getAttribute('title') === 'test title', 'Title did not match' );
+        $this->assertTrue( $link->getAttribute('href') === '#test-anchor', 'Anchor did not match' );
+        $this->assertTrue( $link->getAttribute('target') === '_blank', 'Target did not match' );
+        $this->assertTrue( $link->getAttribute('rel') === 'nofollow', 'Rel did not match' );
+    }
+
+    /**
+     * Test that the default properties match what we expect
+     */
+    public function testDefaultProperties(){
+        $this->objFromFixture('AutomatedLink','link3');
+
+        $page   = $this->createPage('<p>Checking if default is correct</p>');
+        $link   = $this->getLinkFromPage($page);
+        $auto   = AutomatedLink::get()->find('Phrase', 'default');
+
+        $this->assertTrue( $link->nodeValue === 'default', 'Phrase did not match' );
+        $this->assertTrue( $link->getAttribute('title') === '', 'Title did not match' );
+        $this->assertTrue( $link->getAttribute('href') === '#', 'href did not match' );
+        $this->assertTrue( $link->getAttribute('target') === '', 'Target did not match' );
+        $this->assertTrue( $link->getAttribute('rel') === '', 'Rel did not match' );
+        $this->assertTrue( (int) $link->getAttribute('data-id') === $auto->ID, 'data-id did not match' );
+    }
+
+    /**
+     * Test that the default properties match what we expect
+     */
+    public function testLimitFilter(){
+        $this->objFromFixture('AutomatedLink','link3');
+
+        $page   = $this->createPage('<p>Checking if default is created only twice. default default</p>');
         $dom    = $this->getPageDOM($page);
         $links  = $dom->getElementsByTagName('a');
+        $this->assertTrue($links->length == 2, 'Was suppose to find 2 link. Found '.$links->length);
+    }
 
-        $this->assertTrue($links->length == 1);
-
-        if($links->length == 1){
-            $link = $links->item(0);
-            $this->assertTrue( $link->getAttribute('title') == 'title 1' );
-            $this->assertTrue( $link->getAttribute('target') == '_blank' );
-            $this->assertTrue( $link->getAttribute('rel') == '' );
-        }
+    /**
+     * Create a DOMDocument out of the $page supplied and
+     * check that it only has 1 link.
+     * If it does return it, else fail the test
+     *
+     * @param SiteTree $page
+     * @return DOMDocument
+     */
+    private function getLinkFromPage(SiteTree $page){
+        $dom = $this->getPageDOM($page);
+        $links = $dom->getElementsByTagName('a');
+        $this->assertTrue($links->length == 1, 'Didn\'t find the link that was suppose to be created');
+        return ( $links->length != 1 ) ? new DOMElement() : $links->item(0);
     }
 
     /**
